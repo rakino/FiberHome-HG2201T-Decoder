fe4ead1c77a64db65c464b80280dfd9f9ad7bc71
Hilton Chain <26847027+rakino@users.noreply.github.com>
2020-09-01 02:12:39 +0000

    Update fibercat-decoder.py

--- fibercat-decoder.py
+++ fibercat-decoder.py
@@ -1,17 +1,14 @@
 from urllib.request import urlopen
 import json
 
-def passwd_decode(code) -> str: 
+def passwd_decode(code) -> str:
     passwd_list = map(int, code.split('&'))
     result=[]
     for i in passwd_list:
-        '''Use this at your own risk!'''
-        if i <= 57 or i >= 110 and i < 120:
-            pass
-        if i >= 100 and i < 110 or i == 120:
-            i -= 4
-        if i > 57 and i < 100:
+        if 97 <= i <= 100 or 65 <= i <= 68:
             i += 22
+        elif i > 57:
+            i -= 4
 
         result.append(chr(i))
         #print(i, chr(i))
@@ -29,4 +26,5 @@ admin = [baseinfoSet["baseinfoSet_TELECOMACCOUNT"],
 user  = [baseinfoSet["baseinfoSet_USERACCOUNT"],
          baseinfoSet["baseinfoSet_USERPASSWORD"][:-1]]
 
-print (f"Admin: {admin[0]} ; {passwd_decode(admin[1])}\nUser : {user[0]} ; {passwd_decode(user[1])}")
+print ("\n")
+print (f"Admin:\n{admin[0]}\n{passwd_decode(admin[1])}\n\nUser:\n{user[0]}\n{passwd_decode(user[1])}")
